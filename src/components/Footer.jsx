06135671f69216d3f879ae248e16944763f9d5af
Bl Bltich <vissar2016@yandex.ru>
2023-08-14 23:03:59 +0300

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return(
-        <>
-        <footer className="footer">&#169; 2023 Mesto Russia</footer>
-        </>
-    )
-}
